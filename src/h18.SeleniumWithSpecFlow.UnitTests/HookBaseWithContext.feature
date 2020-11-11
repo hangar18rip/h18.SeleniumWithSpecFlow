69d04b0af1a4a278c0bd7b269768adbbcd531a15
Olivier <olivier@odelmotte.fr>
2020-11-11 14:10:05 +0100

    Testing

---
+++ src/h18.SeleniumWithSpecFlow.UnitTests/HookBaseWithContext.feature
@@ -0,0 +1,10 @@
+﻿Feature: HookBaseWithContext
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	When the two numbers are added
+	Then the result should be 120
---
+++ src/h18.SeleniumWithSpecFlow.UnitTests/HookConfigurationBaseTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace h18.SeleniumWithSpecFlow.UnitTests
+{
+    [TestClass]
+    public class HookConfigurationBaseTests
+    {
+        private class FakeConfig : HookConfigurationBase<OpenQA.Selenium.Chrome.ChromeOptions>
+        {
+
+        }
+
+        [TestMethod]
+        public void HookConfigurationBase_ctor()
+        {
+            var fc = new FakeConfig();
+            Assert.IsNotNull(fc);
+
+            fc.BeforeStepScreenShotEnabled = !fc.AfterScenarioScreenShotEnabled;
+            fc.AfterStepScreenShotEnabled = !fc.AfterStepScreenShotEnabled;
+            fc.BeforeScenarioScreenShotEnabled = !fc.BeforeScenarioScreenShotEnabled;
+            fc.AfterScenarioScreenShotEnabled = !fc.AfterScenarioScreenShotEnabled;
+
+            fc.WindowsPosition = new System.Drawing.Point(100, 100);
+            fc.WindowsSize = new System.Drawing.Size(500, 500);
+            fc.WindowsState = WindowsState.FullScreen;
+            var ts = TimeSpan.FromSeconds(2);
+            fc.ImplicitWaitTimeout = ts;
+            fc.AsynchronousJavaScriptTimeout = ts;
+            fc.PageLoadTimeout = ts;
+        }
+
+
+
+    }
+}
